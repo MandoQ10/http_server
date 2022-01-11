6ef702c7d8b5c2a70c4ce01bce5ee3c474bf6bce
MandoQ10 <aquintana@8thlight.com>
2022-01-11 15:35:58 -0600

    Added more tests and files to responseParserTest

--- tests/ResponsesJSONParserTest.java
+++ tests/ResponsesJSONParserTest.java
@@ -1,7 +1,11 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Hashtable;
+
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.Test;
 
@@ -16,29 +20,43 @@ class ResponsesJSONParserTest {
 			+ "because \"responseObject\" is null";
 	
 	@Test
-	void testResponseParser() throws IOException, ParseException  {
-		
+	void testParserWhenResponseExistsWithoutEndPoint() throws IOException, ParseException  {
+		int numberOfEndPoints = 2;
 		EndPointJSONParser parsedRequests = new EndPointJSONParser();
-		parsedRequests.parseEndPointsJSON("src/EndPoints.json", endPoints);
-		
 		ResponsesJSONParser parseResponses = new ResponsesJSONParser();
-		parseResponses.getResponsesForEndPoints("src/Responses.json", endPoints);
-	}
+		
+		parsedRequests.parseEndPointsJSON("tests/files/endPoints.json", endPoints);
+		Throwable exception = assertThrows(Exception.class, () 
+				-> parseResponses.getResponsesForEndPoints("tests/files/responses.json", endPoints));
+		
+		assertEquals(numberOfEndPoints, endPoints.size());		
+		assertEquals("The Response's EndPoint does not exist", exception.getMessage());
+	}	
 	
 	@Test
-	void testEmptyResponseJSON() throws IOException, ParseException  {
-		
+	void testResponsesAreAddedToEndPoints() throws IOException, ParseException  {
+		int numberOfEndPoints = 2;
 		EndPointJSONParser parsedRequests = new EndPointJSONParser();
-		parsedRequests.parseEndPointsJSON("src/EndPoints.json",endPoints);
-		
 		ResponsesJSONParser parseResponses = new ResponsesJSONParser();
 		
+		parsedRequests.parseEndPointsJSON("tests/files/endPoints.json", endPoints);
+		parseResponses.getResponsesForEndPoints("tests/files/responsesWithExactEndPoints.json", endPoints);
+	
+		assertEquals(numberOfEndPoints, endPoints.size());		
+	}	
+		
+	@Test
+	void testInvalidResponses() throws IOException, ParseException  {
+			
+		EndPointJSONParser parsedRequests = new EndPointJSONParser();
+		ResponsesJSONParser parseResponses = new ResponsesJSONParser();
+		
+		parsedRequests.parseEndPointsJSON("src/EndPoints.json",endPoints);
 		Throwable exception = assertThrows(Exception.class, () 
 				-> parseResponses.getResponsesForEndPoints("tests/files/invalidResponse.json", endPoints));
 		
 		assertEquals(jsonAndResponseObjectNull, exception.getMessage());
 	}
-
 
 }	
  
